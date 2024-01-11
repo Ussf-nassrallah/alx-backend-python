62e3869879d9f319ad8bb8a857f075ce36a141a6
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-11 10:12:56 +0100

    first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+  add - returns their sum as a float
+  @a: float
+  @b: float
+  return: sum (float)
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ add: function shold return a float """
+    sum: float = float(a + b)
+    return sum
