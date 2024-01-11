656586efccdc62b23a2f1f9d48e27a288be3ace8
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-11 10:41:23 +0100

    convert float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string """
+
+def to_str(n: float) -> str:
+    ''' to_str: convert float to string '''
+    output: str = str(n)
+    return output
