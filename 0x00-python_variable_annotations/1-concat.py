0d6bed32d67f09fdaf364cfc996aab49e17d8f21
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-11 10:26:19 +0100

    done: concat(str1, str2)

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+  concat - function that takes two strings
+    and returns a concatenated string
+  @str1: first string
+  @str2: second string
+  return: concat string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concat function shold return a string """
+    output: str = str(str1 + str2)
+    return output
