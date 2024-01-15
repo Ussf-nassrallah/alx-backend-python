b3e1477fcde51a83ec309639d05cad54e5879638
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-15 14:47:54 +0100

    fix: task-2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     ''' measure_time '''
     # start recording time
     start_time: float = time.time()
