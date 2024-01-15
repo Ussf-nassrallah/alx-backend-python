860267bcaee22626e15aa5df105286ed8e3c76af
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-15 11:53:28 +0100

    done: task-1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    ''' wait_n '''
     output: List[float] = []
     for idx in range(n):
         value = await wait_random(max_delay)
