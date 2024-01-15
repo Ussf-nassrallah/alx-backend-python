872a0606e0c70cb85443202e351992d190e7c66d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-15 12:18:43 +0100

    done: task-2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+Measure the runtime
+'''
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    ''' measure_time '''
+    # start recording time
+    start = time.time()
+    # run wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
+    # finish recording time
+    end = time.time()
+    # calc total time
+    total = end - start
+    # return total_time / n
+    return total / n
