39e8ae16ffd726acbfc965eec91aea566d81469e
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-04 12:21:05 +0100

    test: utils module get_json method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,9 @@
 ''' test_utils module '''
 
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,3 +27,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ''' test access_nested_map exception '''
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    ''' TestGetJson class '''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests')
+    def test_get_json(self, test_url, test_payload, mock_get_json):
+        ''' test get_json method '''
+        mock_res = MagicMock()
+        mock_res.json.return_value = test_payload
+        mock_get_json.get.return_value = mock_res
+
+        self.assertEqual(get_json(test_url), test_payload)
