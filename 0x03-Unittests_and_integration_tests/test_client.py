9ca93bb3a644c3b243ac1e3894f7873d1bc253ca
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-02-05 10:40:44 +0100

    test: client module (_public_repos method)

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,3 +21,18 @@ class TestGithubOrgClient(unittest.TestCase):
         test_client = GithubOrgClient(org)
         self.assertEqual(test_client.org(), expected)
         mock_org.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+
+    @patch('client.GithubOrgClient')
+    def test_public_repos_url(self, mock_public_repos_url):
+        ''' test _public_repos_url method '''
+        test_input = 'google'
+        expected_output = {
+            'repos_url': "https://api.github.com/orgs/google/repos",
+        }
+
+        mock_res = MagicMock(return_value=expected_output)
+        mock_res.get.return_value = mock_res
+        self.assertEqual(
+            GithubOrgClient(test_input)._public_repos_url,
+            expected_output['repos_url']
+        )
